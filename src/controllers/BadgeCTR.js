76a08e22297e28705c1e908f661050065349ff6e
xbgm08 <gabrielbraulino@gmail.com>
2024-06-26 23:30:48 -0300

    feat(tg-2024): adição do método consultarBadge na BadgeCTR

--- src/controllers/BadgeCTR.js
+++ src/controllers/BadgeCTR.js
@@ -51,3 +51,66 @@ export const cadastrarBadge = async (IMG, DESC, CRIADOR) => {
         }
     }
 }
+
+export const consultarBadge = async (P) => {
+    console.log('Entrei na função consultarBadge() em BadgeController')
+    try{
+        const { data } = await BadgeServer.get('/consultar', {
+            params: {
+                pesquisa: P
+            }
+        })
+        const entregaView = {
+            id_badge: data.id_badge,
+            imagem_mb: data.imagem_mb,
+            desc_certificacao: data.desc_certificacao,
+            criador: data.criador 
+        }
+        return(
+            {
+                sucesso: true,
+                pacote: entregaView
+            }
+        )
+    }
+    catch(err){
+        console.log(err)
+        if(err.response){
+            switch(err.response.data.name){
+                case 'TokenExpirado':
+                    return(
+                        {
+                            sucesso: false,
+                            caso: 'expirado',
+                            pacote: 'Sua sessão expirou. Faça Login novamente para poder continuar suas atividades'
+                        }
+                    )
+                case 'ServicoIndisponivel':
+                    return(
+                        {
+                            sucesso: false,
+                            caso: 'erro',
+                            pacote: {severity: 'error', summary: err.response.data.messsage, detail: 'Tente novamente mais tarde', life: msgLife}
+                        }
+                    )
+                default:
+                    return(
+                        {
+                            sucesso: false,
+                            caso: 'erro',
+                            pacote: { severity: 'error', summary: 'Erro na Consulta', detail: err.response.data.message, life: msgLife }
+                        }
+                    )
+            }
+        }
+        if(err.request){
+            return(
+                {
+                    sucesso: false,
+                    caso: 'erro',
+                    pacote: {severity: 'error', summary: 'Erro Inesperado', detail: 'Tente novamente mais tarde', life: msgLife}
+                }
+            )
+        }
+    }
+}
