6e0415c2678f19db3d7b2ea7a7843a28975ac9aa
Andre <luizzz.z127@gmail.com>
2024-07-29 21:19:50 -0300

    feat (tg-registro): inicio do registro

--- src/views/Cadastro.js
+++ src/views/Cadastro.js
@@ -1,193 +1,69 @@
-// views/Cadastro.js
-import React, { useState } from "react";
-import { Form, Button } from "react-bootstrap";
-import {
-  PersonFill,
-  LockFill,
-  EnvelopeFill,
-  FileText,
-} from "react-bootstrap-icons";
-import PhoneInput from "react-phone-input-2";
-import "react-phone-input-2/lib/style.css";
+import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/Cadastro.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { PersonFill, EnvelopeFill, LockFill } from "react-bootstrap-icons";
 
 const Cadastro = () => {
-  const [userType, setUserType] = useState("comum");
-
-  const renderFields = () => {
-    switch (userType) {
-      case "comum":
-        return (
-          <>
-            <Form.Group controlId="nomeCompleto">
-              <Form.Label>
-                <PersonFill color="#8DFD8B" /> Nome Completo
-              </Form.Label>
-              <Form.Control type="text" placeholder="Nome Completo" />
-            </Form.Group>
-            <Form.Group controlId="ocupacao">
-              <Form.Label>
-                <FileText color="#8DFD8B" /> Ocupação
-              </Form.Label>
-              <Form.Control type="text" placeholder="Ocupação" />
-            </Form.Group>
-            <Form.Group controlId="telefone">
-              <Form.Label>
-                <PhoneInput />
-              </Form.Label>
-            </Form.Group>
-            <Form.Group controlId="email">
-              <Form.Label>
-                <EnvelopeFill color="#8DFD8B" /> Email
-              </Form.Label>
-              <Form.Control type="email" placeholder="Email" />
-            </Form.Group>
-            <Form.Group controlId="senha">
-              <Form.Label>
-                <LockFill color="#8DFD8B" /> Senha
-              </Form.Label>
-              <Form.Control type="password" placeholder="Senha" />
-            </Form.Group>
-            <Form.Group controlId="confirmarSenha">
-              <Form.Label>
-                <LockFill color="#8DFD8B" /> Confirmar Senha
-              </Form.Label>
-              <Form.Control type="password" placeholder="Confirmar Senha" />
-            </Form.Group>
-          </>
-        );
-      case "empresarial":
-        return (
-          <>
-            <Form.Group controlId="emailEmpresarial">
-              <Form.Label>
-                <EnvelopeFill color="#8DFD8B" /> Email Empresarial
-              </Form.Label>
-              <Form.Control type="email" placeholder="Email Empresarial" />
-            </Form.Group>
-            <Form.Group controlId="senhaEmpresarial">
-              <Form.Label>
-                <LockFill color="#8DFD8B" /> Senha
-              </Form.Label>
-              <Form.Control type="password" placeholder="Senha" />
-            </Form.Group>
-            <Form.Group controlId="confirmarSenhaEmpresarial">
-              <Form.Label>
-                <LockFill color="#8DFD8B" /> Confirmar Senha
-              </Form.Label>
-              <Form.Control type="password" placeholder="Confirmar Senha" />
-            </Form.Group>
-            <Form.Group controlId="nomeEmpresa">
-              <Form.Label>
-                <FileText color="#8DFD8B" /> Nome da Empresa
-              </Form.Label>
-              <Form.Control type="text" placeholder="Nome da Empresa" />
-            </Form.Group>
-            <Form.Group controlId="cnpj">
-              <Form.Label>
-                <FileText color="#8DFD8B" /> CNPJ
-              </Form.Label>
-              <Form.Control type="text" placeholder="CNPJ" />
-            </Form.Group>
-            <Form.Group controlId="cep">
-              <Form.Label>
-                <FileText color="#8DFD8B" /> CEP
-              </Form.Label>
-              <Form.Control type="text" placeholder="CEP" />
-            </Form.Group>
-            <Form.Group controlId="endereco">
-              <Form.Label>
-                <FileText color="#8DFD8B" /> Endereço
-              </Form.Label>
-              <Form.Control type="text" placeholder="Endereço" />
-            </Form.Group>
-            <Form.Group controlId="bairro">
-              <Form.Label>
-                <FileText color="#8DFD8B" /> Bairro
-              </Form.Label>
-              <Form.Control type="text" placeholder="Bairro" />
-            </Form.Group>
-            <Form.Group controlId="cidade">
-              <Form.Label>
-                <FileText color="#8DFD8B" /> Cidade
-              </Form.Label>
-              <Form.Control type="text" placeholder="Cidade" />
-            </Form.Group>
-            <Form.Group controlId="complemento">
-              <Form.Label>
-                <FileText color="#8DFD8B" /> Complemento
-              </Form.Label>
-              <Form.Control type="text" placeholder="Complemento" />
-            </Form.Group>
-            <Form.Group controlId="numeroContato">
-              <Form.Label>
-                <PhoneInput />
-              </Form.Label>
-            </Form.Group>
-          </>
-        );
-      case "admin":
-        return (
-          <>
-            <Form.Group controlId="emailAdmin">
-              <Form.Label>
-                <EnvelopeFill color="#8DFD8B" /> Email Administrativo
-              </Form.Label>
-              <Form.Control type="email" placeholder="Email Administrativo" />
-            </Form.Group>
-            <Form.Group controlId="senhaAdmin">
-              <Form.Label>
-                <LockFill color="#8DFD8B" /> Senha
-              </Form.Label>
-              <Form.Control type="password" placeholder="Senha" />
-            </Form.Group>
-            <Form.Group controlId="confirmarSenhaAdmin">
-              <Form.Label>
-                <LockFill color="#8DFD8B" /> Confirmar Senha
-              </Form.Label>
-              <Form.Control type="password" placeholder="Confirmar Senha" />
-            </Form.Group>
-            <Form.Group controlId="nomeAdmin">
-              <Form.Label>
-                <PersonFill color="#8DFD8B" /> Nome
-              </Form.Label>
-              <Form.Control type="text" placeholder="Nome" />
-            </Form.Group>
-            <Form.Group controlId="cargoAdmin">
-              <Form.Label>
-                <FileText color="#8DFD8B" /> Cargo
-              </Form.Label>
-              <Form.Control type="text" placeholder="Cargo" />
-            </Form.Group>
-          </>
-        );
-      default:
-        return null;
-    }
-  };
-
   return (
-    <div className="cadastro-container">
-      <div className="cadastro-inner">
-        <h2 className="cadastro-title">Criar conta</h2>
-        <Form>
-          <Form.Group controlId="tipoUsuario">
-            <Form.Label className="cadastro-label">Tipo de Usuário</Form.Label>
-            <Form.Control
-              as="select"
-              value={userType}
-              onChange={(e) => setUserType(e.target.value)}
-            >
-              <option value="comum">Comum</option>
-              <option value="empresarial">Empresarial</option>
-              <option value="admin">Admin</option>
-            </Form.Control>
-          </Form.Group>
-          {renderFields()}
-          <Button variant="success" type="submit" className="cadastro-button">
-            Registrar
-          </Button>
-        </Form>
+    <div className="cadastro-page">
+      <div className="cadastro-container">
+        <h2>Register</h2>
+        <form>
+          <div className="form-group">
+            <div className="input-icon">
+              <PersonFill />
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                placeholder="Name"
+              />
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="input-icon">
+              <EnvelopeFill />
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                placeholder="Email"
+              />
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="input-icon">
+              <LockFill />
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Password"
+              />
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="input-icon">
+              <LockFill />
+              <input
+                type="password"
+                className="form-control"
+                id="confirmPassword"
+                placeholder="Confirm Password"
+              />
+            </div>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Sign Up
+          </button>
+        </form>
+        <div className="login-options">
+          <span className="have-account">Already have an account?</span>
+          <Link to="/login" className="login-link">
+            Log In
+          </Link>
+        </div>
       </div>
     </div>
   );
